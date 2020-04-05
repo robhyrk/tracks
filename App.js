745465a0fa04c7f822794a2ef05875ce78570921
robhyrk <rob.hyrkiel@gmail.com>
2020-04-05 15:07:14 -0400

    feat: get tracks and list. fixed minor styling

--- App.js
+++ App.js
@@ -17,6 +17,17 @@ import {Provider as LocationProvider } from './src/context/LocationContext'
 import {Provider as TrackProvider} from './src/context/TrackContext'
 
 import {setNavigator} from './src/navigationRef'
+import {FontAwesome} from '@expo/vector-icons'
+
+const trackListFlow = createStackNavigator({
+  TrackList: TrackListScreen,
+  TrackDetail: TrackDetailScreen
+})
+
+trackListFlow.navigationOptions = {
+  title: 'Tracks',
+  tabBarIcon: <FontAwesome name="th-list" size={20}/>
+}
 
 const switchNavigator = createSwitchNavigator({
   ResolveAuthScreen: ResolveAuthScreen,
@@ -25,10 +36,7 @@ const switchNavigator = createSwitchNavigator({
     Signin: SigninScreen
   }),
   mainFlow: createBottomTabNavigator({
-    trackListFlow: createStackNavigator({
-      TrackList: TrackListScreen,
-      TrackDetail: TrackDetailScreen
-    }),
+    trackListFlow,
     TrackCreate: TrackCreateScreen,
     Account: AccountScreen
   })
--- src/screens/TrackCreateScreen.js
+++ src/screens/TrackCreateScreen.js
@@ -8,6 +8,7 @@ import Map from '../components/Map'
 import {Context as LocationContext} from '../context/LocationContext'
 import useLocation from '../hooks/useLocation'
 import TrackForm from '../components/TrackForm'
+import {FontAwesome} from '@expo/vector-icons'
 
 
 const TrackCreateScreen = ({isFocused}) => {
@@ -19,13 +20,17 @@ const TrackCreateScreen = ({isFocused}) => {
 
     return (
     <SafeAreaView forceInset={{top: 'always'}}>
-        <Text h3>Create a Track</Text>
         <Map/>
 
         {err ? <Text>Please enable location services</Text> : null}
         <TrackForm/>
     </SafeAreaView>
     )
+}
+
+TrackCreateScreen.navigationOptions = {
+    title: 'Add Track',
+    tabBarIcon: <FontAwesome name="plus" size={20}/>
 }
 
 const styles = StyleSheet.create({})
